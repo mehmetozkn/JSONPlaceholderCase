4c1c8a0006c503f6ed02d28cefe91475e3fd5a39
Mehmet Özkan <mehmet.trozkan@gmail.com>
2025-02-13 15:56:51 +0300

    app delegate refactor

--- JsonPlaceholderCase/JsonPlaceholderCase/Core/AppDelegate.swift
+++ JsonPlaceholderCase/JsonPlaceholderCase/Core/AppDelegate.swift
@@ -3,13 +3,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow()
-        window?.rootViewController = UserListVC()
-        window?.makeKeyAndVisible()
         return true
     }
 
